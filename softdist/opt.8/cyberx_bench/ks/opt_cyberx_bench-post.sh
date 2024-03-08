3300a7749fcc031806ad047feceb842026ad4f03
bjs <83233973+BJSmithIEEE@users.noreply.github.com>
2024-03-08 14:21:01 -0500

    Add initial opt_cyberx_bench-post.sh

---
+++ softdist/opt.8/cyberx_bench/ks/opt_cyberx_bench-post.sh
@@ -0,0 +1,13 @@
+#
+#       /opt/cyberx_bench/opt_cyberx_bench-post.sh
+#
+
+### GLOBALS
+
+elVerRel="8"
+myOpt="cyberx_bench"	# Tooling
+ADM_USR="1000:1000"	# Administrative User/Group
+
+### Ensure ownership
+[ "${myOpt}" != "" ] && [ -d "/opt/${myOpt}" ] && [ "${ADM_USR}" != "" ] && /bin/chown -R ${ADM_USR} "/opt/${myOpt}"
+
